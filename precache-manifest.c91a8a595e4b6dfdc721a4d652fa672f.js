931b8af687bbfabf7206f2473486458631865a97
pradyoth25 <pradyoth25@gmail.com>
2020-09-24 20:30:29 -0500

    Initial commit

---
+++ precache-manifest.c91a8a595e4b6dfdc721a4d652fa672f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e3f0b23f5d5359372d7f11125c2928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce4ab0bc73e79413f6d0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ac9210d9a04ad25ba73",
+    "url": "/static/js/2.646e2083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.646e2083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce4ab0bc73e79413f6d0",
+    "url": "/static/js/main.5352a439.chunk.js"
+  },
+  {
+    "revision": "bd852e7eb139d6cc279b",
+    "url": "/static/js/runtime-main.50ed05b9.js"
+  }
+]);
