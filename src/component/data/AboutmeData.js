45e0ebcdc06da7f8f473c75323b8235f105fec29
pradyoth25 <pradyoth25@gmail.com>
2021-09-13 16:24:30 -0500

    adding photo

--- src/component/data/AboutmeData.js
+++ src/component/data/AboutmeData.js
@@ -1,7 +1,7 @@
 let aboutmeData = {
-    "about" : "I graduated from Northwestern University, Chicago with a Master of Science (M.S.) in Computer Engineering." +
-    " I am currently working at eBay as a Software Engineer - II in the Core Product Organization. Prior to this I was working at Grubhub as a Software Engineer - II where I did" 
-    + " work with features relating to the Driver Application." +
+    "about" : "I graduated from Northwestern University, Evanston with a Master of Science (M.S.) in Computer Engineering." +
+    " I am currently working at eBay as a Software Engineer - II in the Identity Platform team, which means that my team primarily deals with User Information." +
+    " Prior to this I was working at Grubhub as a Software Engineer - II where I worked on services which interacted with the Driver Application." +
     " I have experience working with Distrbuted Systems and building scalable systems",
     "contact" : {
         "name": "Pradyoth Hegde",
