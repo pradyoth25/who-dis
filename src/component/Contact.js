041cc1bd62e0ea1d7942c8c0b52563f6cbf73403
pradyoth25 <pradyoth25@gmail.com>
2021-11-12 17:05:04 -0600

    fix email issues

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,8 +1,79 @@
 import React, { Component } from 'react'
+import { ReactDOM } from 'react';
+import emailjs from 'emailjs-com';
+import Alert from 'react-popup-alert';
+import { ToastContainer, toast } from 'react-toastify';
+
+const TEMPLATE_ID = 'template_djj9cck';
+const SERVICE_ID = 'service_7p2qbcf';
+const USER_ID = 'user_B5T2cI2ZJz5gWl9frcsnO';
 
 export default class Contact extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {emailSuccess: false}
+    }
+    
+    
     render() {
+        
+        function clearFields(e) {
+            document.getElementById('contactName').value = "";
+            document.getElementById('contactName').disabled=true;
+            document.getElementById('contactName').style.backgroundColor = "#CCCCCC";
+
+            document.getElementById('contactEmail').value = "";
+            document.getElementById('contactEmail').disabled=true;
+            document.getElementById('contactEmail').style.backgroundColor = "#CCCCCC";
+
+            document.getElementById('contactSubject').value = "";
+            document.getElementById('contactSubject').disabled=true;
+            document.getElementById('contactSubject').style.backgroundColor = "#CCCCCC";
+
+            document.getElementById('contactMessage').value = "";
+            document.getElementById('contactMessage').disabled=true;
+            document.getElementById('contactMessage').style.backgroundColor = "#CCCCCC";
+            return (<div id="message-success">
+                    <i className="fa fa-check" />Your message was sent, thank you!<br />
+                    </div>)
+
+        }
+
+        function successEmail(params) {
+            return (<div id="message-success">
+                        <i className="fa fa-check" />Your message was sent, thank you!<br />
+                        </div>)
+        }
+
+        function failureEmail(params) {
+            return (<div id="message-warning"> Error boy</div>)
+        }
+
+        function onClickTest(e) {
+            console.log("AIDSSDSDSFDSDSDSDS clicked HERE");
+        }
+
+        function sendEmail(e) {
+            e.preventDefault();
+            console.log("ONSUBMIT!!!!!!!!");
+            emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+                .then((result) => {
+                    console.log(result.text);
+                    clearFields();
+                    // Popup.alert('Your message was sent, thank you!');
+                    window.alert('Your message was sent, thank you!');
+                }, (error) => {
+                    console.log(error.text);
+                    // Popup.alert('Error boy');
+                    window.alert('Error boy');
+                });
+
+            return 
+
+        }
+
+
         return (
             <React.Fragment>
 
@@ -23,26 +94,28 @@ export default class Contact extends Component {
                     <div className="row">
                         <div className="eight columns">
                             {/* form */}
-                            <form action method="post" id="contactForm" name="contactForm">
+                            <form 
+                            // action method="post" 
+                            id="contactForm" name="contactForm" onSubmit={sendEmail}>
                                 <fieldset>
                                     <div>
                                         <label htmlFor="contactName">Name <span className="required">*</span></label>
-                                        <input type="text" placeholder="First Last" size={35} id="contactName" name="contactName" />
+                                        <input type="text" placeholder="First Last" size={35} id="contactName" name="contactName" required/>
                                     </div>
                                     <div>
                                         <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                                        <input type="text" placeholder="name@email-address.com" size={35} id="contactEmail" name="contactEmail" />
+                                        <input type="text" placeholder="name@email-address.com" size={35} id="contactEmail" name="contactEmail" required/>
                                     </div>
                                     <div>
                                         <label htmlFor="contactSubject">Subject</label>
-                                        <input type="text" placeholder="Subject" size={35} id="contactSubject" name="contactSubject" />
+                                        <input type="text" placeholder="Subject" size={35} id="contactSubject" name="contactSubject" required/>
                                     </div>
                                     <div>
                                         <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                                        <textarea cols={50} rows={15} id="contactMessage" name="contactMessage" defaultValue={""} />
+                                        <textarea cols={50} rows={15} id="contactMessage" name="contactMessage" defaultValue={""} required/>
                                     </div>
                                     <div>
-                                        <button className="submit">Submit</button>
+                                        <button className="submit" onClick={onClickTest}>Submit</button>
                                         <span id="image-loader">
                                             <img alt="" src="images/loader.gif" />
                                         </span>
